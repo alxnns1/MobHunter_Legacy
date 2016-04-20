b31c16bee3eec03da61a4e9c9d4315ef349b55ad
alxnns1 <alxnns1@live.co.uk>
2016-04-20 17:18:02 +0100

    ref class
    added reference class

---
+++ src/main/java/com/alxnns1/mobhunter/reference/Reference.java
@@ -0,0 +1,10 @@
+package com.alxnns1.mobhunter.reference;
+
+/**
+ * Created by Alex on 20/04/2016.
+ */
+public class Reference {
+    public static final String MOD_ID = "MobHunter";
+    public static final String MOD_NAME = "Mob Hunter";
+    public static final String VERSION = "1.8.9-1.0";
+}
