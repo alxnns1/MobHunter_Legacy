82a11322fa7fd10a5ebce5288207f7dab437457d
bright_spark <markpassey@btinternet.com>
2016-04-21 17:52:26 +0100

    Ore generation
    Created code to generate ore in general, but implemented it for the two
    current ores.
    Machalite has the same spawning as iron ore, and Earth Crystals have the
    same spawning as coal ore.

--- src/main/java/com/alxnns1/mobhunter/MobHunter.java
+++ src/main/java/com/alxnns1/mobhunter/MobHunter.java
@@ -4,12 +4,14 @@ import com.alxnns1.mobhunter.init.MHBlocks;
 import com.alxnns1.mobhunter.init.MHItems;
 import com.alxnns1.mobhunter.init.MHRecipes;
 import com.alxnns1.mobhunter.reference.Reference;
+import com.alxnns1.mobhunter.worldgen.WorldGenHandler;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 
 /**
@@ -56,6 +58,7 @@ public class MobHunter {
         }
 
         MHRecipes.init();
+        GameRegistry.registerWorldGenerator(new WorldGenHandler(), 0);
     }
 
     @Mod.EventHandler
--- src/main/java/com/alxnns1/mobhunter/worldgen/WorldGenHandler.java
+++ src/main/java/com/alxnns1/mobhunter/worldgen/WorldGenHandler.java
@@ -1,7 +1,10 @@
 package com.alxnns1.mobhunter.worldgen;
 
+import com.alxnns1.mobhunter.init.MHBlocks;
+import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
 import java.util.Random;
@@ -15,6 +18,68 @@ public class WorldGenHandler implements IWorldGenerator
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
     {
+        switch(world.provider.getDimensionId())
+        {
+            case -1:
+                //Nether
+                break;
+            case 0:
+                //Overworld
+                genOverworld(world, random, chunkX, chunkZ);
+                break;
+            case 1:
+                //End
+                break;
+        }
+    }
 
+    private void genOverworld(World world, Random random, int chunkX, int chunkZ)
+    {
+        //WorldGenMineable(block state, number of blocks to spawn)
+        WorldGenMinable oreEarthCrystal = new WorldGenMinable(MHBlocks.blockOreEarthCrystal.getDefaultState(), 17);
+        genOre(world, random, chunkX, chunkZ, 20, oreEarthCrystal, 0, 128);
+        WorldGenMinable oreMachalite = new WorldGenMinable(MHBlocks.blockOreMachalite.getDefaultState(), 9);
+        genOre(world, random, chunkX, chunkZ, 20, oreMachalite, 0, 64);
+    }
+
+    /**
+     * Generates ores into the world
+     * @param world World object
+     * @param random Random object
+     * @param chunkX Chunk X coord
+     * @param chunkZ Chunk Z coord
+     * @param genCount How many veins to generate per chunk
+     * @param generator The WorldGenMinable object for the ore
+     * @param minHeight The minimum height the veins will spawn at
+     * @param maxHeight The maximum height the veins will spawn at
+     */
+    private void genOre(World world, Random random, int chunkX, int chunkZ, int genCount, WorldGenMinable generator, int minHeight, int maxHeight)
+    {
+        //Some vanilla checks for the height limits
+        if (maxHeight < minHeight)
+        {
+            int i = minHeight;
+            minHeight = maxHeight;
+            maxHeight = i;
+        }
+        else if (maxHeight == minHeight)
+        {
+            if (minHeight < 255)
+            {
+                ++maxHeight;
+            }
+            else
+            {
+                --minHeight;
+            }
+        }
+
+        for(int i = 0; i < genCount; i++)
+        {
+            //Creates random position in chunk for vein
+            BlockPos pos = new BlockPos((chunkX * 16) + random.nextInt(16), random.nextInt(maxHeight - minHeight) + minHeight, (chunkZ * 16) + random.nextInt(16));
+            //Generates vein
+            generator.generate(world, random, pos);
+        }
     }
 }
