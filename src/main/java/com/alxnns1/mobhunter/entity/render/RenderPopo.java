5ec0ae6d670e4bf079354e1fd49c7be3d4e5b81f
alxnns1 <alxnns1@live.co.uk>
2016-04-22 12:51:57 +0100

    scaling models
    added scaling to models of popo

--- src/main/java/com/alxnns1/mobhunter/entity/render/RenderPopo.java
+++ src/main/java/com/alxnns1/mobhunter/entity/render/RenderPopo.java
@@ -5,7 +5,10 @@ import net.minecraft.client.model.ModelCow;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderCow;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.passive.EntityCow;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
+import org.lwjgl.opengl.GL11;
 
 /**
  * Created by Mark on 22/04/2016.
@@ -26,5 +29,16 @@ public class RenderPopo extends RenderCow
         {
             return new RenderPopo(manager);
         }
+    }
+
+    @Override
+    protected void preRenderCallback(EntityCow entity, float f)
+    {
+        preRenderCallbackPopo((EntityPopo) entity, f);
+    }
+
+    protected void preRenderCallbackPopo(EntityPopo entity, float f)
+    {
+        GL11.glScalef(2,2,2); //sizemult will replace the 2s
     }
 }
