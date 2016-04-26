def980e3570763650f1606d2e1e4c262a9c21ead
thebrightspark <markpassey@btinternet.com>
2016-04-26 12:57:34 +0100

    Added furnace recipes for steaks
    Also removed the recipe for bone mail which I missed.

--- src/main/java/com/alxnns1/mobhunter/init/MHRecipes.java
+++ src/main/java/com/alxnns1/mobhunter/init/MHRecipes.java
@@ -15,13 +15,14 @@ public class MHRecipes
     @SuppressWarnings("all")
     public static void init()
     {
-        GameRegistry.addRecipe(new ItemStack(MHItems.itemBoneMail), new Object[]{"xyx", "yxy", "xyx", 'x', Items.string, 'y', Items.bone});
-
         GameRegistry.addRecipe(new ItemStack(MHItems.armourBoneHelmet), new Object[] {"xyx", " z ", "   ", 'x', MHItems.itemMysteryBone, 'y', new ItemStack(Items.skull,1,0), 'z', Items.string});
         GameRegistry.addRecipe(new ItemStack(MHItems.armourBoneChestplate), new Object[] {"w w", "xyx", "xzx", 'w', MHItems.itemMonsterBoneS, 'x', MHItems.itemMysteryBone, 'y', Items.leather, 'z', Items.string});
         GameRegistry.addRecipe(new ItemStack(MHItems.armourBoneLeggings), new Object[] {"xxx", "y y", "z z", 'x', Items.leather, 'y', MHItems.itemMysteryBone, 'z', MHItems.itemMonsterBoneS});
         GameRegistry.addRecipe(new ItemStack(MHItems.armourBoneBoots), new Object[] {"   ", "y y", "z z", 'y', MHItems.itemMonsterBoneS, 'z', MHItems.itemMysteryBone});
 
         GameRegistry.addShapelessRecipe(new ItemStack(Items.skull,1,0), new ItemStack(Items.skull,1, OreDictionary.WILDCARD_VALUE));
+
+        GameRegistry.addSmelting(MHItems.itemRawMeat, new ItemStack(MHItems.itemRareSteak), 0.2f);
+        GameRegistry.addSmelting(MHItems.itemRareSteak, new ItemStack(MHItems.itemDoneSteak), 0.2f);
     }
 }
