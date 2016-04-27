6ae573b4d5340210c3b6bda3c2d85be9c47dc2c1
bright_spark <markpassey@btinternet.com>
2016-04-27 19:24:26 +0100

    RenderPopo now extends RenderLiving rather than RenderCow

--- src/main/java/com/alxnns1/mobhunter/entity/render/RenderPopo.java
+++ src/main/java/com/alxnns1/mobhunter/entity/render/RenderPopo.java
@@ -3,9 +3,8 @@ package com.alxnns1.mobhunter.entity.render;
 import com.alxnns1.mobhunter.entity.EntityPopo;
 import com.alxnns1.mobhunter.entity.models.ModelPopo;
 import net.minecraft.client.renderer.entity.Render;
-import net.minecraft.client.renderer.entity.RenderCow;
+import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 import org.lwjgl.opengl.GL11;
@@ -13,32 +12,35 @@ import org.lwjgl.opengl.GL11;
 /**
  * Created by Mark on 22/04/2016.
  */
-public class RenderPopo extends RenderCow {
+public class RenderPopo extends RenderLiving<EntityPopo>
+{
     public static final Factory FACTORY = new Factory();
     private static final ResourceLocation popoTextures = new ResourceLocation("mobhunter:textures/entities/popo.png");
 
-    public RenderPopo(RenderManager renderManagerIn) {
+    public RenderPopo(RenderManager renderManagerIn)
+    {
         super(renderManagerIn, new ModelPopo(), 0.7f);
     }
 
-    public static class Factory implements IRenderFactory<EntityPopo> {
+    @Override
+    protected ResourceLocation getEntityTexture(EntityPopo entity)
+    {
+        return popoTextures;
+    }
+
+    public static class Factory implements IRenderFactory<EntityPopo>
+    {
         @Override
-        public Render<? super EntityPopo> createRenderFor(RenderManager manager) {
+        public Render<? super EntityPopo> createRenderFor(RenderManager manager)
+        {
             return new RenderPopo(manager);
         }
     }
 
     @Override
-    protected void preRenderCallback(EntityCow entity, float f) {
-        preRenderCallbackPopo((EntityPopo) entity, f);
-    }
-
-    protected void preRenderCallbackPopo(EntityPopo entity, float f) {
+    protected void preRenderCallback(EntityPopo entity, float f)
+    {
         float tSizeMult = entity.getPopoScale();
         GL11.glScalef(tSizeMult,tSizeMult,tSizeMult);
-    }
-
-    protected ResourceLocation getEntityTexture(EntityCow entity) {
-        return popoTextures;
     }
 }
