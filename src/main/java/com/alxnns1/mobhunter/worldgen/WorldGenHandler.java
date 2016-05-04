97deac63df44e046e84628a745e0763c32ea85fd
bright_spark <markpassey@btinternet.com>
2016-05-04 14:51:20 +0100

    Added world generation for the herb block

--- src/main/java/com/alxnns1/mobhunter/worldgen/WorldGenHandler.java
+++ src/main/java/com/alxnns1/mobhunter/worldgen/WorldGenHandler.java
@@ -4,6 +4,7 @@ import com.alxnns1.mobhunter.init.MHBlocks;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.GeneratorBushFeature;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
@@ -15,6 +16,12 @@ import java.util.Random;
  */
 public class WorldGenHandler implements IWorldGenerator
 {
+    //WorldGenMineable(block state, number of blocks to spawn)
+    WorldGenMinable oreEarthCrystal = new WorldGenMinable(MHBlocks.blockOreEarthCrystal.getDefaultState(), 17);
+    WorldGenMinable oreMachalite = new WorldGenMinable(MHBlocks.blockOreMachalite.getDefaultState(), 9);
+
+    GeneratorBushFeature bushHerb = new GeneratorBushFeature(MHBlocks.blockHerb);
+
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
     {
@@ -22,24 +29,35 @@ public class WorldGenHandler implements IWorldGenerator
         {
             case -1:
                 //Nether
-                break;
-            case 0:
-                //Overworld
-                genOverworld(world, random, chunkX, chunkZ);
+                genBushes(world, random, chunkX, chunkZ);
                 break;
             case 1:
                 //End
                 break;
+            default:
+                //Overworld and whatever else
+                genOverworld(world, random, chunkX, chunkZ);
+                break;
         }
+    }
+
+    private BlockPos getRandXZInChunk(Random random, int chunkX, int chunkZ)
+    {
+        return new BlockPos(chunkX * 16 + random.nextInt(16), 0, chunkZ * 16 + random.nextInt(16));
     }
 
     private void genOverworld(World world, Random random, int chunkX, int chunkZ)
     {
-        //WorldGenMineable(block state, number of blocks to spawn)
-        WorldGenMinable oreEarthCrystal = new WorldGenMinable(MHBlocks.blockOreEarthCrystal.getDefaultState(), 17);
         genOre(world, random, chunkX, chunkZ, 20, oreEarthCrystal, 0, 128);
-        WorldGenMinable oreMachalite = new WorldGenMinable(MHBlocks.blockOreMachalite.getDefaultState(), 9);
         genOre(world, random, chunkX, chunkZ, 20, oreMachalite, 0, 64);
+
+        genBushes(world, random, chunkX, chunkZ);
+    }
+
+    private void genBushes(World world, Random random, int chunkX, int chunkZ)
+    {
+        BlockPos pos = getRandXZInChunk(random, chunkX, chunkZ);
+        bushHerb.generate(world, random, world.getHeight(pos));
     }
 
     /**
@@ -77,7 +95,7 @@ public class WorldGenHandler implements IWorldGenerator
         for(int i = 0; i < genCount; i++)
         {
             //Creates random position in chunk for vein
-            BlockPos pos = new BlockPos((chunkX * 16) + random.nextInt(16), random.nextInt(maxHeight - minHeight) + minHeight, (chunkZ * 16) + random.nextInt(16));
+            BlockPos pos = getRandXZInChunk(random, chunkX, chunkZ).add(0, random.nextInt(maxHeight - minHeight) + minHeight, 0);
             //Generates vein
             generator.generate(world, random, pos);
         }
