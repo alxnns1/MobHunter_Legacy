37de1c481cc2dbe822be9bb5c4c9d9d17a993d19
alxnns1 <alxnns1@live.co.uk>
2016-05-10 13:37:50 +0100

    Finalising Herb block and Mushroom block
    -Herb and Mushroom blocks now drop items depending on what they're grown
    on
    -Fixed Mushroom blocks not spawning on non-grass blocks
    -Fixed localised name of Hot Pepper

--- src/main/java/com/alxnns1/mobhunter/block/BlockHerb.java
+++ src/main/java/com/alxnns1/mobhunter/block/BlockHerb.java
@@ -3,10 +3,14 @@ package com.alxnns1.mobhunter.block;
 import com.alxnns1.mobhunter.MobHunter;
 import com.alxnns1.mobhunter.init.MHItems;
 import com.alxnns1.mobhunter.reference.Names;
+import com.alxnns1.mobhunter.util.LogHelper;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockBush;
+import net.minecraft.block.BlockNetherrack;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.ColorizerGrass;
@@ -53,22 +57,86 @@ public class BlockHerb extends BlockBush {
     @Override
     public ArrayList<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState blockstate, int fortune) {
         Block blockUnderneath = world.getBlockState(pos.down()).getBlock();
-
         ArrayList<ItemStack> drops = new ArrayList<ItemStack>();
-        for(int n=0;n<RANDOM.nextInt(4)+1+fortune;n++){
-            int i = RANDOM.nextInt(5);
-            if(i==0) {
-                drops.add(new ItemStack(MHItems.itemHerb));
-            }else if(i==1){
-                drops.add(new ItemStack(MHItems.itemAntidoteHerb));
-            }else if(i==2){
-                drops.add(new ItemStack(MHItems.itemSleepHerb));
-            }else if(i==3){
-                drops.add(new ItemStack(MHItems.itemSapPlant));
-            }else if(i==4){
-                drops.add(new ItemStack(MHItems.itemFireHerb));
-            }else {
-                drops.add(new ItemStack(MHItems.itemHotPepper));
+        for(int n=0;n<RANDOM.nextInt(2)+1+fortune;n++){
+            double i = RANDOM.nextDouble();
+            if(blockUnderneath.getMaterial()==Material.sand) {
+                //On top of sand and gravel
+                if (i<0.24044694515556929348449815838642) {
+                    drops.add(new ItemStack(MHItems.itemHerb));
+                } else if (i<0.39824025291391164233370007482751) {
+                    drops.add(new ItemStack(MHItems.itemFireHerb));
+                } else if (i<0.49731329976041936048240533453302) {
+                    drops.add(new ItemStack(MHItems.itemSleepHerb));
+                } else if (i<0.63162546053091314551476172769419) {
+                    drops.add(new ItemStack(MHItems.itemSapPlant));
+                } else if (i<0.75964119522022087121252695090918) {
+                    //drops.add(new ItemStack(MHItems.itemFelvine));
+                } else if (i<0.8934382856696908812966428616242) {
+                    //drops.add(new ItemStack(MHItems.itemGloamgrassRoot));
+                } else {
+                    drops.add(new ItemStack(MHItems.itemHotPepper));
+                }
+            }else if(blockUnderneath==Blocks.netherrack || blockUnderneath==Blocks.nether_brick || blockUnderneath==Blocks.soul_sand) {
+                //On top of nether blocks
+                if (i<0.15722379603399433427762039660058) {
+                    drops.add(new ItemStack(MHItems.itemHerb));
+                } else if (i<0.41643059490084985835694050991504) {
+                    drops.add(new ItemStack(MHItems.itemAntidoteHerb));
+                } else if (i<0.58640226628895184135977337110485) {
+                    drops.add(new ItemStack(MHItems.itemFireHerb));
+                } else if (i<0.75637393767705382436260623229466) {
+                    //drops.add(new ItemStack(MHItems.itemIvy));
+                } else if (i<0.84135977337110481586402266288957) {
+                    drops.add(new ItemStack(MHItems.itemSapPlant));
+                } else {
+                    //drops.add(new ItemStack(MHItems.itemGloamgrassRoot));
+                }
+            }else if(blockUnderneath.getMaterial()==Material.snow || blockUnderneath.getMaterial()==Material.craftedSnow || blockUnderneath.getMaterial()==Material.ice || blockUnderneath.getMaterial()==Material.packedIce){
+                //On top of snow and ice
+                if (i<0.23785803237858032378580323785803) {
+                    drops.add(new ItemStack(MHItems.itemHerb));
+                } else if (i<0.46699875466998754669987546699875) {
+                    drops.add(new ItemStack(MHItems.itemSleepHerb));
+                } else if (i<0.65130759651307596513075965130759) {
+                    drops.add(new ItemStack(MHItems.itemSapPlant));
+                } else {
+                    //drops.add(new ItemStack(MHItems.itemFelvine));
+                }
+            }else if(blockUnderneath.getMaterial()==Material.rock) {
+                //On top of stone and rock material outliers
+                if (i<0.13405341021372206656650559683816) {
+                    drops.add(new ItemStack(MHItems.itemHerb));
+                } else if (i<0.31612999282630175552577794020114) {
+                    drops.add(new ItemStack(MHItems.itemAntidoteHerb));
+                } else if (i<0.50129261921197601548435998429908) {
+                    drops.add(new ItemStack(MHItems.itemFireHerb));
+                } else if (i<0.63837793207996643250632774326282) {
+                    //drops.add(new ItemStack(MHItems.itemIvy));
+                } else if (i<0.75954575601304800963711915106726) {
+                    drops.add(new ItemStack(MHItems.itemSapPlant));
+                } else if (i<0.84400590138195205803927938170843) {
+                    //drops.add(new ItemStack(MHItems.itemFelvine));
+                } else {
+                    //drops.add(new ItemStack(MHItems.itemGloamgrassRoot));
+                }
+            }else{
+                //On top of grass and other outliers
+                if (i<0.15908241598812579991386447291226) {
+                    drops.add(new ItemStack(MHItems.itemHerb));
+                } else if (i<0.35843886134033408081858071111877) {
+                    drops.add(new ItemStack(MHItems.itemAntidoteHerb));
+                } else if (i<0.48309057811764829542106967932635) {
+                    //drops.add(new ItemStack(MHItems.itemIvy));
+                } else if (i<0.59136575706185465777724422736793) {
+                    drops.add(new ItemStack(MHItems.itemSleepHerb));
+                } else if (i<0.77048450265861149198405745351408) {
+                    drops.add(new ItemStack(MHItems.itemSapPlant));
+                } else if (i<0.8508311912399560415608067252761) {
+                    //drops.add(new ItemStack(MHItems.itemFelvine));
+                } else {
+                    drops.add(new ItemStack(MHItems.itemHotPepper));
+                }
             }
         }
         return drops;
--- src/main/java/com/alxnns1/mobhunter/block/BlockShroom.java
+++ src/main/java/com/alxnns1/mobhunter/block/BlockShroom.java
@@ -3,14 +3,17 @@ package com.alxnns1.mobhunter.block;
 import com.alxnns1.mobhunter.MobHunter;
 import com.alxnns1.mobhunter.init.MHItems;
 import com.alxnns1.mobhunter.reference.Names;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockBush;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.ColorizerGrass;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.EnumPlantType;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -50,23 +53,66 @@ public class BlockShroom extends BlockBush {
 
     @Override
     public ArrayList<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState blockstate, int fortune) {
+        Block blockUnderneath = world.getBlockState(pos.down()).getBlock();
         ArrayList<ItemStack> drops = new ArrayList<ItemStack>();
         for(int n=0;n<RANDOM.nextInt(2)+1+fortune;n++){
-            int i = RANDOM.nextInt(5);
-            if(i==0) {
-                drops.add(new ItemStack(MHItems.itemBlueMushroom));
-            }else if(i==1){
-                drops.add(new ItemStack(MHItems.itemNitroshroom));
-            }else if(i==2){
-                drops.add(new ItemStack(MHItems.itemParashroom));
-            }else if(i==3){
-                drops.add(new ItemStack(MHItems.itemToadstool));
-            }else if(i==4){
-                drops.add(new ItemStack(MHItems.itemExciteshroom));
+            double i = RANDOM.nextDouble();
+            if(blockUnderneath.getMaterial()==Material.sand) {
+                //On top of sand and gravel
+                if (i<0.23469387755102040816326530612244) {
+                    drops.add(new ItemStack(MHItems.itemBlueMushroom));
+                } else if (i<0.60969387755102040816326530612245) {
+                    drops.add(new ItemStack(MHItems.itemExciteshroom));
+                } else {
+                    //drops.add(new ItemStack(MHItems.itemMopeshroom));
+                }
+            }else if(blockUnderneath== Blocks.netherrack || blockUnderneath==Blocks.nether_brick || blockUnderneath==Blocks.soul_sand) {
+                //On top of nether blocks
+                if (i<0.51228978007761966364812419146184) {
+                    drops.add(new ItemStack(MHItems.itemBlueMushroom));
+                } else if (i<0.70177708177299652754136311023354) {
+                    drops.add(new ItemStack(MHItems.itemNitroshroom));
+                } else if (i<0.88765574998297814393681487029346) {
+                    drops.add(new ItemStack(MHItems.itemParashroom));
+                } else {
+                    drops.add(new ItemStack(MHItems.itemExciteshroom));
+                }
+            }else if(blockUnderneath.getMaterial()==Material.rock) {
+                //On top of stone and rock material outliers
+                if (i<0.27414600312120686665510664123462) {
+                    drops.add(new ItemStack(MHItems.itemBlueMushroom));
+                } else if (i<0.43367435408357898387376452228196) {
+                    drops.add(new ItemStack(MHItems.itemNitroshroom));
+                } else if (i<0.5932027050459511010924224033293) {
+                    drops.add(new ItemStack(MHItems.itemParashroom));
+                } else if (i<0.65215883474943644876018727241201) {
+                    drops.add(new ItemStack(MHItems.itemExciteshroom));
+                } else if (i<0.95283509623721172186578810473385) {
+                    //drops.add(new ItemStack(MHItems.itemMopeshroom));
+                } else {
+                    drops.add(new ItemStack(MHItems.itemDragonToadstool));
+                }
             }else {
-                drops.add(new ItemStack(MHItems.itemDragonToadstool));
+                //On top of grass and other outliers
+                if (i<0.14694647912640992225767312272503) {
+                    drops.add(new ItemStack(MHItems.itemBlueMushroom));
+                } else if (i<0.3015323776569451309935739001483) {
+                    drops.add(new ItemStack(MHItems.itemParashroom));
+                } else if (i<0.71675729115175481957488877904105) {
+                    drops.add(new ItemStack(MHItems.itemToadstool));
+                } else if (i<0.79764526131308138228553453466951) {
+                    drops.add(new ItemStack(MHItems.itemExciteshroom));
+                } else {
+                    //drops.add(new ItemStack(MHItems.itemMopeshroom));
+                }
             }
         }
         return drops;
+    }
+
+    @Override
+    public EnumPlantType getPlantType(net.minecraft.world.IBlockAccess world, BlockPos pos)
+    {
+        return EnumPlantType.Cave;
     }
 }
