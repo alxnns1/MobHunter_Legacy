185d1ffb38437cceb7e1e1c3f301807b90f84f27
alxnns1 <alxnns1@live.co.uk>
2016-06-09 23:51:45 +0100

    v0.1.4.1
    -Changed version numbers

--- src/main/java/com/alxnns1/mobhunter/reference/Reference.java
+++ src/main/java/com/alxnns1/mobhunter/reference/Reference.java
@@ -6,7 +6,7 @@ package com.alxnns1.mobhunter.reference;
 public class Reference {
     public static final String MOD_ID = "MobHunter";
     public static final String MOD_NAME = "Mob Hunter";
-    public static final String VERSION = "1.8.9-0.1.4";
+    public static final String VERSION = "1.8.9-0.1.4.1";
 
     public static final String GUI_TEXTURE_DIR = "textures/gui/";
 }
