ba871460dceffebe32d495f03221461af27e0585
alxnns1 <alxnns1@live.co.uk>
2016-06-17 21:36:44 +0100

    Error Fixing
    -Fixed errors in entity model files

--- src/main/java/com/alxnns1/mobhunter/entity/models/ModelGargwa.java
+++ src/main/java/com/alxnns1/mobhunter/entity/models/ModelGargwa.java
@@ -2,9 +2,8 @@ package com.alxnns1.mobhunter.entity.models;
 
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.MathHelper;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
