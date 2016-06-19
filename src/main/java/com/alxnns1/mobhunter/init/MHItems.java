ca91fbf0a1e6a090270e5a6f555568916b3fca30
alxnns1 <alxnns1@live.co.uk>
2016-06-19 21:54:50 +0100

    Undo
    -Undid change in MHItems

--- src/main/java/com/alxnns1/mobhunter/init/MHItems.java
+++ src/main/java/com/alxnns1/mobhunter/init/MHItems.java
@@ -5,7 +5,6 @@ import com.alxnns1.mobhunter.reference.Names;
 import com.alxnns1.mobhunter.reference.Reference;
 import com.alxnns1.mobhunter.util.Common;
 import net.minecraft.item.ItemArmor;
-import net.minecraft.util.SoundEvent;
 import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
