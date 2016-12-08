eefed1b47c64439afedffeec08d56aca1a1e2497
bright_spark <markpassey@btinternet.com>
2016-12-08 13:17:24 +0000

    Updated Forge mappings and added JEI to build.gradle - Created files necessary for JEI integration

---
+++ src/main/java/com/alxnns1/mobhunter/integration/jei/ItemCraftingTables/ItemCraftingRecipeCategory.java
@@ -0,0 +1,8 @@
+package com.alxnns1.mobhunter.integration.jei.ItemCraftingTables;
+
+/**
+ * Created by Mark on 08/12/2016.
+ */
+public class ItemCraftingRecipeCategory
+{
+}
---
+++ src/main/java/com/alxnns1/mobhunter/integration/jei/MobHunterPlugin.java
@@ -0,0 +1,8 @@
+package com.alxnns1.mobhunter.integration.jei;
+
+/**
+ * Created by Mark on 08/12/2016.
+ */
+public class MobHunterPlugin
+{
+}
