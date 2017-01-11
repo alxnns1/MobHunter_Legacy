99fea2464d73392441d009e03a5528376f52bb1d
alxnns1 <alxnns1@live.co.uk>
2017-01-11 17:41:01 +0000

    JEI textures
    -added JEI textures for weapon and armour crafting tables
    -more work on plantable crops

--- src/main/java/com/alxnns1/mobhunter/item/ItemMHConsumable.java
+++ src/main/java/com/alxnns1/mobhunter/item/ItemMHConsumable.java
@@ -7,6 +7,7 @@ import com.alxnns1.mobhunter.reference.MetaRef;
 import com.alxnns1.mobhunter.reference.Names;
 import com.alxnns1.mobhunter.util.CommonUtil;
 import com.alxnns1.mobhunter.util.LogHelper;
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
@@ -15,10 +16,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumHand;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -40,6 +38,8 @@ public class ItemMHConsumable extends ItemFood implements ISubTypes<ItemMHConsum
     private int eatDuration = this.itemUseDuration;
     public static int EAT_DURATION_SHORT = 8;
     protected String[] subNames;
+    public String type;
+    public Block crops;
 
     public ItemMHConsumable(String itemName, String... subNames)
     {
@@ -57,6 +57,7 @@ public class ItemMHConsumable extends ItemFood implements ISubTypes<ItemMHConsum
     {
         super(amount, saturation, isWolfFood);
         setCreativeTab(MobHunter.MH_TAB);
+        this.type = itemName;
         setUnlocalizedName(itemName);
         setRegistryName(itemName);
         setHasSubtypes(subNames != null && subNames.length > 0);
@@ -219,6 +220,24 @@ public class ItemMHConsumable extends ItemFood implements ISubTypes<ItemMHConsum
             subItems.add(new ItemStack(itemIn));
     }
 
+    /**
+     * Called when a Block is right-clicked with this Item
+     */
+    public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        net.minecraft.block.state.IBlockState state = worldIn.getBlockState(pos);
+        if (facing == EnumFacing.UP && playerIn.canPlayerEdit(pos.offset(facing), facing, stack) && state.getBlock().canSustainPlant(state, worldIn, pos, EnumFacing.UP, this) && worldIn.isAirBlock(pos.up()))
+        {
+            worldIn.setBlockState(pos.up(), this.crops.getDefaultState());
+            --stack.stackSize;
+            return EnumActionResult.SUCCESS;
+        }
+        else
+        {
+            return EnumActionResult.FAIL;
+        }
+    }
+
     @Override
     public EnumPlantType getPlantType(IBlockAccess world, BlockPos pos) {
         return null;
@@ -226,18 +245,6 @@ public class ItemMHConsumable extends ItemFood implements ISubTypes<ItemMHConsum
 
     @Override
     public IBlockState getPlant(IBlockAccess world, BlockPos pos) {
-
-        String[] itemNameSplit = this.getUnlocalizedName().split("\\W");
-        String itemName = itemNameSplit[itemNameSplit.length - 1];
-
-        if (    itemName.equals(Names.Items.NITROSHROOM) ||
-                itemName.equals(Names.Items.PARASHROOM) ||
-                itemName.equals(Names.Items.TOADSTOOL) ||
-                itemName.equals(Names.Items.EXCITESHROOM) ||
-                itemName.equals(Names.Items.MOPESHROOM) ||
-                itemName.equals(Names.Items.DRAGON_TOADSTOOL)){
-            return null;
-        }
         return null;
     }
 
