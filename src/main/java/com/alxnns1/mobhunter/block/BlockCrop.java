752e61531e08eaf5e89aa8929081046fda28f219
alxnns1 <alxnns1@live.co.uk>
2017-02-19 21:00:52 +0000

    Cleaning
    Cleaned up code and fixed some bugs in preperation for 1.10

--- src/main/java/com/alxnns1/mobhunter/block/BlockCrop.java
+++ src/main/java/com/alxnns1/mobhunter/block/BlockCrop.java
@@ -1,10 +1,12 @@
 package com.alxnns1.mobhunter.block;
 
 import com.alxnns1.mobhunter.MobHunter;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
@@ -29,7 +31,6 @@ public class BlockCrop extends BlockCrops {
         seed = seedIn;
         setUnlocalizedName(name);
         setRegistryName(name);
-        setCreativeTab(MobHunter.MHBLOCK_TAB);
     }
 
     protected ItemStack getSeed(IBlockState state) {
@@ -38,6 +39,79 @@ public class BlockCrop extends BlockCrops {
 
     protected ItemStack getCrop(IBlockState state) {
         return crop.copy();
+    }
+
+    protected static float getGrowthChance(Block blockIn, World worldIn, BlockPos pos) {
+        float f = 1.0F;
+        BlockPos blockpos = pos.down();
+
+        for (int i = -1; i <= 1; ++i) {
+            for (int j = -1; j <= 1; ++j) {
+                float f1 = 0.0F;
+                IBlockState iblockstate = worldIn.getBlockState(blockpos.add(i, 0, j));
+
+                if (iblockstate.getBlock()==Blocks.FARMLAND || iblockstate.getBlock()==Blocks.LOG || iblockstate.getBlock()==Blocks.LOG2 || iblockstate.getBlock()==Blocks.MYCELIUM) {
+                    f1 = 1.0F;
+
+                    if (iblockstate.getBlock().isFertile(worldIn, blockpos.add(i, 0, j))) {
+                        f1 = 3.0F;
+                    }
+                }
+
+                if (i != 0 || j != 0) {
+                    f1 /= 4.0F;
+                }
+
+                f += f1;
+            }
+        }
+
+        BlockPos blockpos1 = pos.north();
+        BlockPos blockpos2 = pos.south();
+        BlockPos blockpos3 = pos.west();
+        BlockPos blockpos4 = pos.east();
+        boolean flag = blockIn == worldIn.getBlockState(blockpos3).getBlock() || blockIn == worldIn.getBlockState(blockpos4).getBlock();
+        boolean flag1 = blockIn == worldIn.getBlockState(blockpos1).getBlock() || blockIn == worldIn.getBlockState(blockpos2).getBlock();
+
+        if (flag && flag1) {
+            f /= 2.0F;
+        } else {
+            boolean flag2 = blockIn == worldIn.getBlockState(blockpos3.north()).getBlock() || blockIn == worldIn.getBlockState(blockpos4.north()).getBlock() || blockIn == worldIn.getBlockState(blockpos4.south()).getBlock() || blockIn == worldIn.getBlockState(blockpos3.south()).getBlock();
+
+            if (flag2) {
+                f /= 2.0F;
+            }
+        }
+
+        return f;
+    }
+
+    @Override
+    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
+    {
+        super.updateTick(worldIn, pos, state, rand);
+
+        if (worldIn.getLightFromNeighbors(pos.up()) >= 9)
+        {
+            int i = this.getAge(state);
+
+            if (i < this.getMaxAge())
+            {
+                float f = getGrowthChance(this, worldIn, pos);
+
+                if (rand.nextInt((int)(25.0F / f) + 1) == 0)
+                {
+                    worldIn.setBlockState(pos, this.withAge(i + 1), 2);
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean canBlockStay(World worldIn, BlockPos pos, IBlockState state) {
+        IBlockState soil = worldIn.getBlockState(pos.down());
+
+        return (soil.getBlock()==Blocks.FARMLAND || soil.getBlock()==Blocks.LOG || soil.getBlock()==Blocks.LOG2 || soil.getBlock()==Blocks.MYCELIUM);
     }
 
     @Override
@@ -62,7 +136,7 @@ public class BlockCrop extends BlockCrops {
      */
     @Override
     public Item getItemDropped(IBlockState state, Random rand, int fortune) {
-        return null;//this.isMaxAge(state) ? this.getCrop(state).getItem() : this.getSeed(state).getItem();
+        return null;
     }
 
     @Override
